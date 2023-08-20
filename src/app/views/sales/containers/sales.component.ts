f7aa5d4dc306c806ebf2571dcf705ec8c6250684
Kamal Abd El-Kader <kamal.abdel-kader@digi360.tech>
2023-08-20 19:15:39 +0300

    c

--- src/app/views/sales/containers/sales.component.ts
+++ src/app/views/sales/containers/sales.component.ts
@@ -1,6 +1,8 @@
+import { userType } from "./../../../shared/model/userMangerEnum";
+import { AuthService } from "./../../../shared/services/auth.service";
+import { SessionService } from "./../../../shared/services/session.service";
 import { Category } from "./../../account-statment/model/Category";
 import { CategoryService } from "./../../../shared/services/product/product.service";
-import { TotalSales } from "./../models/total-sales";
 import { SalesService } from "../services/sales.service";
 import { ViewChild, AfterViewInit } from "@angular/core";
 import { Component, OnInit } from "@angular/core";
@@ -10,8 +12,9 @@ import { MatTableDataSource } from "@angular/material/table";
 import { Sales } from "../models/sales";
 import { FormGroup, FormControl } from "@angular/forms";
 import {
-  CloneObject,
+  getAllCatgoryObj,
   IsNullOrEmptyString,
+  ObjectHasValue,
 } from "../../../shared/helper/helper";
 
 @Component({
@@ -27,16 +30,18 @@ export class SalesComponent implements OnInit, AfterViewInit {
     "saleValue",
     "profit",
     "groupName",
-    "date"
+    "date",
   ];
   accountStatment$: any;
   categories: Category[] = [];
+  userType = userType;
   dataSource: MatTableDataSource<Sales> = new MatTableDataSource([]);
   form = new FormGroup({
     fromDate: new FormControl(""),
     toDate: new FormControl(""),
     categoryName: new FormControl(""),
   });
+  isUserAdmin: boolean;
 
   get salesList(): Sales[] {
     return this.getSalesList();
@@ -63,7 +68,9 @@ export class SalesComponent implements OnInit, AfterViewInit {
 
   constructor(
     private salesService: SalesService,
-    private categoService: CategoryService
+    private categoService: CategoryService,
+    private sessionService: SessionService,
+    private authService: AuthService
   ) {}
 
   async ngOnInit() {
@@ -72,14 +79,12 @@ export class SalesComponent implements OnInit, AfterViewInit {
     await this.getAll();
   }
 
-  async getAll() { 
+  async getAll() {
     if (this.categories && this.categories.length > 0) {
       const response = await this.salesService.getAll().toPromise();
       this.dataSource.data = response;
       this.form.get("categoryName")?.setValue(this.categories[0].name);
       this.applyFilter();
-    } else {
-      this.dataSource.data = [];
     }
   }
 
@@ -87,6 +92,7 @@ export class SalesComponent implements OnInit, AfterViewInit {
     this.categories = await this.categoService
       .getCategoryiesByUserAccess()
       .toPromise();
+    this.appendAllCategoryIfUserIsAdmin();
   }
 
   ngAfterViewInit() {
@@ -111,21 +117,30 @@ export class SalesComponent implements OnInit, AfterViewInit {
     data: Sales,
     category: any
   ): boolean {
-    if (fromDate && toDate && !IsNullOrEmptyString(category) && category) {
-      return date >= fromDate && date <= toDate && data.groupName == category;
-    }
+    if (IsNullOrEmptyString(category)) {
+      return false;
+    } else {
+      if (fromDate && toDate && !IsNullOrEmptyString(category) && category) {
+        if (fromDate && toDate && (category as string).toLowerCase() == "all") {
+          return date >= fromDate && date <= toDate;
+        }
+        return date >= fromDate && date <= toDate && data.groupName == category;
+      }
 
-    if (fromDate && toDate && category) {
-      return date >= fromDate && date <= toDate;
-    }
+      if (fromDate && toDate && category) {
+        return date >= fromDate && date <= toDate;
+      }
 
-    return data.groupName == category;
+      if ((category as string).toLowerCase() == "all") {
+        return true;
+      }
+
+      return data.groupName == category;
+    }
   }
 
   applyFilter() {
-    
     this.dataSource.filter = "" + Math.random();
-
   }
 
   getSalesList(): Sales[] {
@@ -137,5 +152,19 @@ export class SalesComponent implements OnInit, AfterViewInit {
     return this.salesList
       .map((items) => items[propName])
       .reduce((prev, curr) => prev + curr, 0);
+  }
+
+  private appendAllCategoryIfUserIsAdmin() {
+    if (this.authService.isLogin()) {
+      const user = this.sessionService.getUser();
+      if (ObjectHasValue(user)) {
+        const keys = Object.keys(this.userType);
+        keys.forEach((key) => {
+          if (user.userType == key) {
+            this.categories = [getAllCatgoryObj(), ...this.categories];
+          }
+        });
+      }
+    }
   }
 }
